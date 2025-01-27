d9b3409d1bf24bc00fd2203b4b25ae77d60464ff
Domiciano <guilhermedomicianosilva@gmail.com>
2025-01-27 11:26:07 -0300

    dataUsuarios retirada

--- src/app/data/constants/usuarios.ts
+++ src/app/data/constants/usuarios.ts
@@ -1,21 +1,6 @@
 import { Usuario } from "@/core/model/Usuario";
 
 const usuarios: Usuario[] = [
-    {
-        id: '1',
-        nome: 'Giorno Domiciano Feitosa',
-        login: 'giorno',
-        senha: '123',
-        empresas: [],
-        UsuarioEmpresas: []
-    }, {
-        id: '2',
-        nome: 'Gaira Domiciano Feitosa',
-        login: 'gaira',
-        senha: '123',
-        empresas: [],
-        UsuarioEmpresas: []
-    }
 ]
 
 export default usuarios;
