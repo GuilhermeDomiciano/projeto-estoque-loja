4428bc731b9c1baabaaf9069e891dc12be5cb8d9
Geisbelly <geisbelly19@gmail.com>
2025-01-28 19:44:13 -0300

    autentificação login

--- auth.ts
+++ auth.ts
@@ -1,5 +1,40 @@
 import NextAuth from "next-auth";
- 
+import Credentials from "next-auth/providers/credentials";
+import db from './lib/db'; // Certifique-se de apontar para o módulo correto
+import bcrypt from "bcrypt";
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
-})
+  providers: [
+    Credentials({
+      credentials: {
+        username: { label: "Usuário", type: "text" },
+        password: { label: "Senha", type: "password" },
+      },
+      authorize: async (credentials) => {
+        // Buscar o usuário no banco de dados pelo login
+        const user = await db.usuario.findFirst({
+          where: { login: String(credentials.username) },
+        });
+
+        if (!user) {
+          console.log("Usuário não encontrado.");
+          return null;
+        }
+
+        // Verificar a senha fornecida em relação ao hash armazenado
+        const isPasswordValid = bcrypt.compareSync(
+          String(credentials.password),
+          user.senha
+        );
+
+        if (isPasswordValid) {
+          console.log("Login bem-sucedido!");
+          return { id: String(user.id), name: user.nome, login: user.login }; 
+        } else {
+          console.log("Senha incorreta.");
+          return null;
+        }
+      },
+    }),
+  ],
+});
