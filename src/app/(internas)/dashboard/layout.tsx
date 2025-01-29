30b337a3d5ee0ee713090e2f4c54e0f76f864b0e
Geisbelly <geisbelly19@gmail.com>
2025-01-28 22:49:00 -0300

    Proteção das rotas

---
+++ src/app/(internas)/dashboard/layout.tsx
@@ -0,0 +1,28 @@
+import { redirect } from "next/navigation";
+import { auth } from "../../../../auth";
+
+
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+    const session = await auth();
+  
+    if(!session){
+      redirect('/login');
+    }
+  
+  return (
+
+    <html lang="en">
+      
+      <body
+        
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
