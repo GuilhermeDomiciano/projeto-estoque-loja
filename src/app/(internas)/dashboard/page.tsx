98d648400cd7802d497a451e8f0b1bb4c9aab01a
Domiciano <guilhermedomicianosilva@gmail.com>
2025-01-29 19:10:42 -0300

    vercel arrumado

--- src/app/(internas)/dashboard/page.tsx
+++ src/app/(internas)/dashboard/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import Backend from "@/backend";
-import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
 export default function Page() {
@@ -9,7 +7,6 @@ export default function Page() {
 
   return (
     <div>
-      {/* <ListarEmpresas empresas={empresas}/> */}
       <button 
         onClick={() => router.push("/cadastrarEmpresa")} 
         className="px-4 py-2"
