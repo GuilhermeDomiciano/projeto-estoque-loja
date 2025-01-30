32cc6fa5de7c517296267fc61e4550bf63280bc8
Domiciano <guilhermedomicianosilva@gmail.com>
2025-01-29 22:24:34 -0300

    arrumando vercel 2

--- src/app/(internas)/dashboard/layout.tsx
+++ src/app/(internas)/dashboard/layout.tsx
@@ -1,7 +1,6 @@
 import { redirect } from "next/navigation";
 import { auth } from "../../../../auth";
 import { getUser } from "../../../../lib/usuario";
-import React from "react";
 
 
 export default async function RootLayout({
@@ -9,17 +8,19 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const session = await auth();
-  const user = await getUser();
+    const session = await auth();
+    const user = await getUser()
   
-  if(!session){
-    redirect('/login');
-  }
+    if(!session){
+      redirect('/login');
+    }
   
+    
   return (
     <html lang="en">
       <body>
-        {React.cloneElement(children as React.ReactElement, user)}
+      ID: {user?.id}
+        {children}
       </body>
     </html>
   );
--- src/app/(internas)/dashboard/page.tsx
+++ src/app/(internas)/dashboard/page.tsx
@@ -2,37 +2,18 @@
 
 import { useRouter } from "next/navigation";
 
-interface User {
-  id: number;
-  nome: string;
-  login: string;
-}
 
-// Aqui definimos a interface corretamente para o componente receber as props
-interface PageProps {
-  user: User;
-}
-
-export default function Page({ user }: PageProps) {
+export default  function Page() {
   const router = useRouter();
-
-  // Função que mostra o ID do usuário
-  const verEmpresa = () => {
-    console.log("ID do Usuário:", user?.id);
-  };
+  
 
   return (
     <div>
-      <button
-        onClick={() => router.push("/cadastrarEmpresa")}
+      <button 
+        onClick={() => router.push("/cadastrarEmpresa")} 
         className="px-4 py-2"
       >
         Cadastrar Empresa
-      </button>
-
-      {/* Chamando a função dentro de um evento (exemplo de onClick) */}
-      <button onClick={verEmpresa} className="px-4 py-2 mt-4">
-        Ver Empresa
       </button>
     </div>
   );
