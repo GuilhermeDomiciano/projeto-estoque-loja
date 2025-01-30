3eb06902a5fbe3ddd0d85553c629daa6958e82c4
Domiciano <guilhermedomicianosilva@gmail.com>
2025-01-30 09:19:09 -0300

    vai 3????

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,15 +1,9 @@
+import Login from "@/app/components/login/Login";
 
-import Login from "@/app/components/login/login";
-
-// import { auth } from "../../../../auth";
-// import { redirect } from "next/navigation";
-
-
-export default  function Home() {
+export default function Home() {
   return (
     <div>
       <Login />
     </div>
   );
 }
-
