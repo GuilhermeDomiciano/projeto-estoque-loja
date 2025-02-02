7618796a263490eac45ca51b63a75d93b0f10bc5
Domiciano <guilhermedomicianosilva@gmail.com>
2025-02-01 22:40:29 -0300

    Baby i want to fly

--- src/app/(internas)/cadastrarEmpresa/page.tsx
+++ src/app/(internas)/cadastrarEmpresa/page.tsx
@@ -11,16 +11,12 @@ import { useEffect, useState } from "react";
 
 export default function Page() {
   const [empresas, setEmpresas] = useState<Empresa[]>([]);
-  const [cargos, setCargos] = useState<Cargo[]>([]);
-  const [usuarioEmpresas, setUsuarioEmpresas] = useState<UsuarioEmpresa[]>([]);
   const { data: session } = useSession();
   const user = session?.user;
   const usuarioId = user?.id ? Number(user.id) : null; 
 
   useEffect(() => {
     Backend.empresas.obterTodas().then(empresas => setEmpresas(empresas || []));
-    Backend.cargos.obterTodos().then(cargos => setCargos(cargos || []));
-    Backend.usuarioEmpresa.obterTodosUsuarioEmpresas().then(usuarioEmpresas => setUsuarioEmpresas(usuarioEmpresas || []));
   }, []);
 
   return (
