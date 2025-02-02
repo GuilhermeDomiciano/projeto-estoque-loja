287c3bf018a60d99d7a969b1be59a80e2fc90e60
Domiciano <guilhermedomicianosilva@gmail.com>
2025-02-02 15:39:51 -0300

    cadastro de marca fix

--- src/app/(internas)/catalogo/page.tsx
+++ src/app/(internas)/catalogo/page.tsx
@@ -1,13 +1,19 @@
 'use client'
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Modal from "../../../components/ui/Modal";
 import CadastroForm from "../../../components/forms/FormCadastro";
+import { Marca } from "@/core/model/Marca";
+import Backend from "@/backend";
+import FormularioMarcas from "@/components/forms/FormMarca";
+import { useSession } from "next-auth/react";
 
 export default function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
-
   const handleOpenModal = () => setIsModalOpen(true);
   const handleCloseModal = () => setIsModalOpen(false);
+  const [marcas, setMarcas] = useState<Marca[]>([]);
+  const {data: session} = useSession()
+  const empresa_id = Number(session?.user?.empresa?.id)
 
   const handleSave = (data: Record<string, string>) => {
     console.error("Formulário enviado com os dados:", data);
@@ -15,6 +21,9 @@ export default function App() {
    //handleCloseModal(); // Fechar o modal após salvar
   };
 
+  useEffect(() => {
+    Backend.marcas.obterTodasMarcas().then(marcas => setMarcas(marcas || []))
+  }, []);
 
   return (
     <div>
@@ -31,7 +40,9 @@ export default function App() {
       >
         <CadastroForm  handleSave={handleSave} onClose={handleCloseModal} />
       </Modal>
+
+      <FormularioMarcas marcas={marcas} setMarcas={setMarcas} usuario_id={empresa_id}/>
     </div>
-    
+
   );
 }
--- src/backend/marca/RepositorioMarca.ts
+++ src/backend/marca/RepositorioMarca.ts
@@ -8,24 +8,27 @@ export default class RepositorioMarca {
     const marcas = await this.db.marca.findMany({
       include: {
         produtos: true,
+        empresa: true,  
       },
     });
 
-    return marcas.map(marca =>({
+    return marcas.map(marca => ({
       ...marca,
       id: Number(marca.id),
-      produtos: marca.produtos.map(produto => produto.id)
+      empresa_Id: marca.empresa_id,  // Adiciona o id da empresa
+      produtos: marca.produtos.map(produto => produto.id),
     }));
   }
 
   static async salvarMarca(marca: Marca): Promise<Marca> {
-    const { produtos = [], ...marcaSemId} = marca;
+    const { produtos = [], empresa_Id, ...marcaSemId } = marca; 
 
     const createdMarca = await this.db.marca.create({
       data: {
         ...marcaSemId,
+        empresa_id: empresa_Id, 
         produtos: {
-          connect: produtos.map((produtoId) => ({id: produtoId})),
+          connect: produtos.map((produtoId) => ({ id: produtoId })),
         },
       },
     });
@@ -33,6 +36,7 @@ export default class RepositorioMarca {
     return {
       ...createdMarca,
       produtos,
-    }
+      empresa_Id,
+    };
   }
 }
