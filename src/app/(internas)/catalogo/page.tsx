f93aca8850340711984cb28c627e79c70d2b56c5
Domiciano <guilhermedomicianosilva@gmail.com>
2025-02-02 15:42:41 -0300

    catalogo

--- src/app/(internas)/catalogo/page.tsx
+++ src/app/(internas)/catalogo/page.tsx
@@ -1,48 +1,47 @@
-'use client'
-import { useEffect, useState } from "react";
+'use client';
+
+import { useState, useEffect } from "react";
 import Modal from "../../../components/ui/Modal";
-import CadastroForm from "../../../components/forms/FormCadastro";
-import { Marca } from "@/core/model/Marca";
-import Backend from "@/backend";
-import FormularioMarcas from "@/components/forms/FormMarca";
-import { useSession } from "next-auth/react";
+import CadastroForm from "../../../components/forms/FormProduto";
 
 export default function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkScreenSize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    checkScreenSize();
+    window.addEventListener("resize", checkScreenSize);
+    return () => window.removeEventListener("resize", checkScreenSize);
+  }, []);
+
   const handleOpenModal = () => setIsModalOpen(true);
   const handleCloseModal = () => setIsModalOpen(false);
-  const [marcas, setMarcas] = useState<Marca[]>([]);
-  const {data: session} = useSession()
-  const empresa_id = Number(session?.user?.empresa?.id)
 
   const handleSave = (data: Record<string, string>) => {
     console.error("Formulário enviado com os dados:", data);
-    // Lógica de salvar os dados
-   //handleCloseModal(); // Fechar o modal após salvar
   };
-
-  useEffect(() => {
-    Backend.marcas.obterTodasMarcas().then(marcas => setMarcas(marcas || []))
-  }, []);
 
   return (
     <div>
-      <button
-        onClick={handleOpenModal}
-        className="px-4 py-2 bg-blue-600 text-white rounded-md"
-      >
-        Abrir Modal de Cadastro
-      </button>
+      {!isMobile && (
+        <button
+          onClick={handleOpenModal}
+          className="px-4 py-2 bg-blue-600 text-white rounded-md"
+        >
+          Abrir Modal de Cadastro
+        </button>
+      )}
 
-      <Modal
-        isOpen={isModalOpen}
-        onClose={handleCloseModal}
-      >
-        <CadastroForm  handleSave={handleSave} onClose={handleCloseModal} />
-      </Modal>
-
-      <FormularioMarcas marcas={marcas} setMarcas={setMarcas} usuario_id={empresa_id}/>
+      {isMobile ? (
+        <CadastroForm handleSave={handleSave} onClose={() => {}} />
+      ) : (
+        <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+          <CadastroForm handleSave={handleSave} onClose={handleCloseModal} />
+        </Modal>
+      )}
     </div>
-
   );
 }
