88db6efbf78e2fc47a304c4f6d9a31860ad0dc18
Domiciano <guilhermedomicianosilva@gmail.com>
2025-02-01 22:38:49 -0300

    o lobo pede auuuuuuuuuuuuuu

--- src/app/(internas)/cadastrarEmpresa/page.tsx
+++ src/app/(internas)/cadastrarEmpresa/page.tsx
@@ -1,9 +1,6 @@
 "use client";
 
-import ListarCargos from "@/components/cargo/ListarCargos";
 import FormularioEmpresas from "@/components/empresa/FormularioEmpresa";
-import ListarEmpresas from "@/components/empresa/ListarEmpresas";
-import ListarUsuarioEmpresa from "@/components/usuarioEmpresa/ListaUsuarioEmpresas";
 import Backend from "@/backend";
 import { Cargo } from "@/core/model/Cargo";
 import { Empresa } from "@/core/model/Empresa";
